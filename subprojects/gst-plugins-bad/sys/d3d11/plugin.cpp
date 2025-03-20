459a3939e503a88c45d653f5b51b763e3e5e5bfb
smomin7 <smomin@vicon-security.com>
2025-03-20 19:37:02 +0530

    Added GstD3d11crop and GstD2d11d2d1 plugins.

--- subprojects/gst-plugins-bad/sys/d3d11/plugin.cpp
+++ subprojects/gst-plugins-bad/sys/d3d11/plugin.cpp
@@ -67,6 +67,7 @@
 #include "gstd3d11download.h"
 #include "gstd3d11convert.h"
 #include "gstd3d11compositor.h"
+#include "gstd3d11crop.h"
 #include "gstd3d11h264dec.h"
 #include "gstd3d11h265dec.h"
 #include "gstd3d11vp9dec.h"
@@ -87,6 +88,10 @@
 using namespace Microsoft::WRL;
 /* *INDENT-ON* */
 
+#ifdef HAVE_D2D1
+#include "gstd3d11d2d1.h"
+#endif
+
 GST_DEBUG_CATEGORY (gst_d3d11_debug);
 GST_DEBUG_CATEGORY (gst_d3d11_plugin_utils_debug);
 GST_DEBUG_CATEGORY (gst_d3d11_format_debug);
@@ -94,6 +99,7 @@ GST_DEBUG_CATEGORY (gst_d3d11_device_debug);
 GST_DEBUG_CATEGORY (gst_d3d11_overlay_compositor_debug);
 GST_DEBUG_CATEGORY (gst_d3d11_window_debug);
 GST_DEBUG_CATEGORY (gst_d3d11_video_processor_debug);
+GST_DEBUG_CATEGORY (gst_d3d11_crop_debug);
 GST_DEBUG_CATEGORY (gst_d3d11_decoder_debug);
 GST_DEBUG_CATEGORY (gst_d3d11_h264_dec_debug);
 GST_DEBUG_CATEGORY (gst_d3d11_h265_dec_debug);
@@ -108,6 +114,12 @@ GST_DEBUG_CATEGORY (gst_d3d11_screen_capture_debug);
 GST_DEBUG_CATEGORY (gst_d3d11_screen_capture_device_debug);
 #endif
 
+
+
+#ifdef HAVE_D2D1
+GST_DEBUG_CATEGORY(gst_d3d11_d2d1_debug);
+#endif
+
 #define GST_CAT_DEFAULT gst_d3d11_debug
 
 static gboolean
@@ -119,6 +131,8 @@ plugin_init (GstPlugin * plugin)
   ComPtr < IDXGIFactory1 > factory;
 
   GST_DEBUG_CATEGORY_INIT (gst_d3d11_debug, "d3d11", 0, "direct3d 11 plugin");
+  GST_DEBUG_CATEGORY_INIT(gst_d3d11_crop_debug,
+      "d3d11crop", 0, "d3d11crop");
   GST_DEBUG_CATEGORY_INIT (gst_d3d11_plugin_utils_debug,
       "d3d11pluginutils", 0, "d3d11 plugin utility functions");
   GST_DEBUG_CATEGORY_INIT (gst_d3d11_overlay_compositor_debug,
@@ -156,6 +170,11 @@ plugin_init (GstPlugin * plugin)
   hr = CreateDXGIFactory1 (IID_PPV_ARGS (&factory));
   if (FAILED (hr))
     return TRUE;
+
+#ifdef HAVE_D2D1
+  GST_DEBUG_CATEGORY_INIT(gst_d3d11_d2d1_debug,
+      "d3d11d2d1", 0, "d3d11d2d1 element");
+#endif
 
   /* Enumerate devices to register decoders per device and to get the highest
    * feature level */
@@ -227,6 +246,8 @@ plugin_init (GstPlugin * plugin)
       "d3d11colorconvert", GST_RANK_NONE, GST_TYPE_D3D11_COLOR_CONVERT);
   gst_element_register (plugin,
       "d3d11scale", GST_RANK_NONE, GST_TYPE_D3D11_SCALE);
+  gst_element_register(plugin,
+      "d3d11crop", GST_RANK_NONE, GST_TYPE_D3D11CROP);
   gst_element_register (plugin,
       "d3d11videosink", video_sink_rank, GST_TYPE_D3D11_VIDEO_SINK);
 
@@ -249,6 +270,11 @@ plugin_init (GstPlugin * plugin)
         "d3d11screencapturedeviceprovider", GST_RANK_PRIMARY,
         GST_TYPE_D3D11_SCREEN_CAPTURE_DEVICE_PROVIDER);
   }
+#endif
+
+#ifdef HAVE_D2D1
+  gst_element_register(plugin,
+      "d3d11d2d1", GST_RANK_NONE, GST_TYPE_D3D11D2D1);
 #endif
 
   return TRUE;
