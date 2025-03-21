smomin7/gstreamer

commit 6c66ede6db9952d196da0eba91146ad5fe0235fe

aggregator: fix start-time-selection=first on negative rate
When the property "start-time-selection" is set to "first", it
calculates the start time of the output from the buffer pts
(converting it to running time of the segment), but if the
rate is negative, the real start is not the pts, but the
pts + duration, because it plays from the end of the buffer
to it's start.
As a result of this bug, in the negative rate, when the
start-time-selection=first, the first frame is dropped
by the videoaggregator (reproduced on d3d11compositor).

subprojects/gstreamer/libs/gst/base/gstaggregator.c
@@ -3190,6 +3190,10 @@ gst_aggregator_pad_chain_internal (GstAggregator * self,
         if (aggpad->priv->head_segment.format == GST_FORMAT_TIME) {
           start_time = buf_pts;
           if (start_time != -1) {
+            GstClockTime buf_duration = GST_BUFFER_DURATION (buffer);
+            if (aggpad->priv->head_segment.rate < 0.0 && buf_duration != -1) {
+              start_time += buf_duration;
+            }
             start_time = MAX (start_time, aggpad->priv->head_segment.start);
             start_time =
                 gst_segment_to_running_time (&aggpad->priv->head_segment,

commit 1334c780079347b6639198662f687a602fe38eff

aggregator: Update include following commits
commit: 447da9a6105b66591b9c9677c63b06af75a11cac
* aggregator: handle erroneous case of the first buffer before segment
Segment event always should arrive before the first buffer, however if
that happens due to some complex pipelines manipulations, we don't want
the process to crash or go into errouneous state.
In partigular an abort gets raised by the videoaggregator in this case.
To avoid that we propose to safely assume we have received a segment in
which the first buffer is valid.

commit: 35f740b09f9d10493af1030904478ed3b035c129
* aggregator: avoid race condition after FLUSH_STOP event
We wait until all pads have buffers in gst_aggregator_wait_and_check()
but ub case of FLUSH_STOP event first_buffer was not getting true which
is one of the condition to wait in gst_aggregator_wait_and _check()
causing fail to wait

commit: dccb2f6b5c293c2ddad76c79da74cec288b85988
* aggregator: fix use-after-free in queries processing
Test included.
The problem appears when aggregator drops the query while
it's being proccessed by the klass->sink_query handler.
This can happen on FLUSH_START event. If the query is still
in the queue, it can be safely dropped, but if it's already
in the klass->sink_query() handler, then sink pad has no
choice and has to wait for the proccessing to complete.

commit: 7829a5e5e8d2cbd964c5b9add27a1dfd96161442
* Revert "aggregator: fix use-after-free in queries processing"
This reverts commit 3943b39fee9b0fb56f3f37572c96e4571b23cc86.

commit: 3943b39fee9b0fb56f3f37572c96e4571b23cc86
* aggregator: fix use-after-free in queries processing
This is a real use-after-free caught by address sanitizer.
The problem is that while the query is proccessed by the
klass->sink_event(), it might be destroyed if there will
be a state change at this moment.

commit: 1fc183726336fcc83aa5b6d2f8aa96d4f5d0ebba
* fix for crash caused by commit 6c66ede6db9952d196da0eba91146ad5fe0235fe

subprojects/gstreamer/libs/gst/base/gstaggregator.c
@@ -264,6 +264,12 @@ struct _GstAggregatorPadPrivate
   guint num_buffers;
   GstBuffer *peeked_buffer;
 
+  /* TRUE if the serialized query is in the proccess of handling at some
+   * exact moment. This will obligate the sinkpad streaming thread wait
+   * until the handling finishes.
+   * Always protected by the PAD_LOCK. */
+  gboolean query_in_proccess;
+
   /* used to track fill state of queues, only used with live-src and when
    * latency property is set to > 0 */
   GstClockTime head_position;
@@ -971,8 +977,10 @@ gst_aggregator_do_events_and_queries (GstElement * self, GstPad * epad,
         !GST_IS_BUFFER (g_queue_peek_tail (&pad->priv->data))) {
       if (GST_IS_EVENT (g_queue_peek_tail (&pad->priv->data)))
         event = gst_event_ref (g_queue_peek_tail (&pad->priv->data));
-      if (GST_IS_QUERY (g_queue_peek_tail (&pad->priv->data)))
+      if (GST_IS_QUERY (g_queue_peek_tail (&pad->priv->data))) {
         query = g_queue_peek_tail (&pad->priv->data);
+        pad->priv->query_in_proccess = TRUE;
+      }
     }
     PAD_UNLOCK (pad);
     if (event || query) {
@@ -1007,6 +1015,8 @@ gst_aggregator_do_events_and_queries (GstElement * self, GstPad * epad,
               NULL);
           g_queue_pop_tail (&pad->priv->data);
         }
+
+        pad->priv->query_in_proccess = FALSE;
       }
 
       PAD_BROADCAST_EVENT (pad);
@@ -1716,6 +1726,9 @@ gst_aggregator_default_sink_event (GstAggregator * self,
         SRC_LOCK (self);
         priv->send_eos = TRUE;
         priv->got_eos_event = FALSE;
+        if (self->priv->start_time_selection ==
+            GST_AGGREGATOR_START_TIME_SELECTION_FIRST)
+          priv->first_buffer = TRUE;
         SRC_BROADCAST (self);
         SRC_UNLOCK (self);
 
@@ -2635,9 +2648,14 @@ gst_aggregator_default_sink_query_pre_queue (GstAggregator * self,
     SRC_BROADCAST (self);
     SRC_UNLOCK (self);
 
-    while (!gst_aggregator_pad_queue_is_empty (aggpad)
-        && aggpad->priv->flow_return == GST_FLOW_OK) {
-      GST_DEBUG_OBJECT (aggpad, "Waiting for buffer to be consumed");
+    /* Sanity check: aggregator's sink pad can only proccess one serialized
+     * query at a time. */
+    g_warn_if_fail (!aggpad->priv->query_in_proccess);
+
+    while ((!gst_aggregator_pad_queue_is_empty (aggpad)
+            && aggpad->priv->flow_return == GST_FLOW_OK) ||
+        aggpad->priv->query_in_proccess) {
+      GST_DEBUG_OBJECT (aggpad, "Waiting for query to be consumed");
       PAD_WAIT_EVENT (aggpad);
     }
 
@@ -3122,6 +3140,7 @@ gst_aggregator_pad_chain_internal (GstAggregator * self,
 {
   GstFlowReturn flow_return;
   GstClockTime buf_pts;
+  GstClockTime buf_duration;
 
   GST_TRACE_OBJECT (aggpad,
       "entering chain internal with %" GST_PTR_FORMAT, buffer);
@@ -3134,7 +3153,7 @@ gst_aggregator_pad_chain_internal (GstAggregator * self,
   PAD_UNLOCK (aggpad);
 
   buf_pts = GST_BUFFER_PTS (buffer);
-
+  buf_duration = GST_BUFFER_DURATION (buffer);
   for (;;) {
     SRC_LOCK (self);
     GST_OBJECT_LOCK (self);
@@ -3187,20 +3206,26 @@ gst_aggregator_pad_chain_internal (GstAggregator * self,
         break;
       case GST_AGGREGATOR_START_TIME_SELECTION_FIRST:
         GST_OBJECT_LOCK (aggpad);
-        if (aggpad->priv->head_segment.format == GST_FORMAT_TIME) {
-          start_time = buf_pts;
-          if (start_time != -1) {
-            start_time = MAX (start_time, aggpad->priv->head_segment.start);
-            start_time =
-                gst_segment_to_running_time (&aggpad->priv->head_segment,
-                GST_FORMAT_TIME, start_time);
-          }
-        } else {
-          start_time = 0;
+        if (aggpad->priv->head_segment.format != GST_FORMAT_TIME) {
+          gst_segment_init (&aggpad->priv->head_segment, GST_FORMAT_TIME);
+          gst_segment_do_seek (&aggpad->priv->head_segment, 1.0, GST_FORMAT_TIME,
+            GST_SEEK_FLAG_ACCURATE, GST_SEEK_TYPE_SET, buf_pts,
+            GST_SEEK_TYPE_SET, GST_CLOCK_TIME_NONE, NULL);
           GST_WARNING_OBJECT (aggpad,
-              "Ignoring request of selecting the first start time "
-              "as the segment is a %s segment instead of a time segment",
-              gst_format_get_name (aggpad->priv->head_segment.format));
+            "No valid segment received before the first buffer. Assuming a segment "
+            "from the buffer pts: %" GST_SEGMENT_FORMAT, &aggpad->priv->head_segment);
+        }
+
+        start_time = buf_pts;
+        if (start_time != -1) {
+          //GstClockTime buf_duration = GST_BUFFER_DURATION (buffer);
+          if (aggpad->priv->head_segment.rate < 0.0 && buf_duration != -1) {
+            start_time += buf_duration;
+          }
+          start_time = MAX(start_time, aggpad->priv->head_segment.start);
+          start_time =
+            gst_segment_to_running_time(&aggpad->priv->head_segment,
+              GST_FORMAT_TIME, start_time);
         }
         GST_OBJECT_UNLOCK (aggpad);
         break;

subprojects/gstreamer/tests/check/libs/aggregator.c
@@ -166,6 +166,24 @@ gst_test_aggregator_aggregate (GstAggregator * aggregator, gboolean timeout)
 #define gst_test_aggregator_parent_class parent_class
 G_DEFINE_TYPE (GstTestAggregator, gst_test_aggregator, GST_TYPE_AGGREGATOR);
 
+static gboolean gst_aggregator_test_slow_down_sink_query = FALSE;
+
+static gboolean
+gst_aggregator_test_slow_sink_query (GstAggregator * self,
+    GstAggregatorPad * aggpad, GstQuery * query)
+{
+  GST_DEBUG ("Handling query %" GST_PTR_FORMAT, query);
+  if (GST_QUERY_IS_SERIALIZED (query)) {
+    GstStructure *s = gst_query_writable_structure (query);
+
+    if (gst_aggregator_test_slow_down_sink_query)
+      g_usleep (G_TIME_SPAN_MILLISECOND * 10);
+    gst_structure_set (s, "some-int", G_TYPE_INT, 123, NULL);
+    GST_DEBUG ("Written to the query %" GST_PTR_FORMAT, query);
+  }
+  return GST_AGGREGATOR_CLASS (parent_class)->sink_query (self, aggpad, query);
+}
+
 static void
 gst_test_aggregator_class_init (GstTestAggregatorClass * klass)
 {
@@ -193,6 +211,7 @@ gst_test_aggregator_class_init (GstTestAggregatorClass * klass)
       GST_DEBUG_FUNCPTR (gst_test_aggregator_aggregate);
 
   base_aggregator_class->get_next_time = gst_aggregator_simple_get_next_time;
+  base_aggregator_class->sink_query = gst_aggregator_test_slow_sink_query;
 }
 
 static void
@@ -642,6 +661,60 @@ GST_START_TEST (test_aggregate_handle_queries)
   _chain_data_clear (&data1);
   _chain_data_clear (&data2);
   _test_data_clear (&test);
+}
+
+GST_END_TEST;
+
+GST_START_TEST (test_aggregate_queries_robustness)
+{
+  GThread *thread1;
+  ChainData data1 = { 0, };
+  TestData test = { 0, };
+  GstCaps *caps;
+  gint64 start_time;
+
+  gst_aggregator_test_slow_down_sink_query = TRUE;
+
+  _test_data_init (&test, FALSE);
+
+  caps = gst_caps_new_empty_simple ("foo/x-bar");
+  _chain_data_init (&data1, test.aggregator,
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE),
+      gst_query_new_allocation (caps, FALSE), NULL);
+  gst_caps_unref (caps);
+
+  thread1 = g_thread_try_new ("gst-check", push_data, &data1, NULL);
+  g_usleep (G_TIME_SPAN_MILLISECOND * 5);
+  for (start_time = g_get_monotonic_time ();
+      start_time + G_TIME_SPAN_SECOND > g_get_monotonic_time ();
+      g_usleep (G_TIME_SPAN_MILLISECOND)) {
+    fail_unless (gst_element_send_event (test.aggregator,
+            gst_event_new_flush_start ()));
+    fail_unless (gst_element_send_event (test.aggregator,
+            gst_event_new_flush_stop (TRUE)));
+  }
+
+  g_thread_join (thread1);
+
+  _chain_data_clear (&data1);
+  _test_data_clear (&test);
+
+  gst_aggregator_test_slow_down_sink_query = FALSE;
 }
 
 GST_END_TEST;
@@ -1398,6 +1471,7 @@ gst_aggregator_suite (void)
   tcase_add_test (general, test_aggregate_gap);
   tcase_add_test (general, test_aggregate_handle_events);
   tcase_add_test (general, test_aggregate_handle_queries);
+  tcase_add_test (general, test_aggregate_queries_robustness);
   tcase_add_test (general, test_flushing_seek);
   tcase_add_test (general, test_infinite_seek);
   tcase_add_test (general, test_infinite_seek_50_src);