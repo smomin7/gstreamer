c5f60882486e99917a56e44591b64f4612fe645e
smomin7 <smomin@vicon-security.com>
2025-03-21 15:33:38 +0530

    gstinfo : Update include commit
    commit: 616c59a8e4dcd0d7d6f4fc2c1340317ab5becb27* [d87363b bhagirathimohanta]WTC-1213-Debug-Crash
    
    commit: c0e439987d1945d501fba242d9d34529baa33514* [cc40516 bhagirathimohanta]WTC-1213
    
    commit: 364a3001e2bb99579a14046dbebdc14584fc2105* docs: Use backticks to escape * in markdownOtherwise it's interpreted as emphasis.Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/4763>

--- subprojects/gstreamer/gst/gstinfo.c
+++ subprojects/gstreamer/gst/gstinfo.c
@@ -391,7 +391,7 @@ void
 _priv_gst_debug_init (void)
 {
   const gchar *env;
-  FILE *log_file;
+  FILE *log_file = NULL;
 
   if (add_default_log_func) {
     env = g_getenv ("GST_DEBUG_FILE");
@@ -618,9 +618,8 @@ gst_debug_log_full_valist (GstDebugCategory * category, GstDebugLevel level,
   G_VA_COPY (message.arguments, args);
 
   handler = __log_functions;
-  while (handler) {
+  if (handler) {
     entry = handler->data;
-    handler = g_slist_next (handler);
     entry->func (category, level, file, function, line, object, &message,
         entry->user_data);
   }
@@ -1788,7 +1787,9 @@ gst_debug_remove_with_compare_func (GCompareFunc func, gpointer data)
 
     if (entry->notify)
       entry->notify (entry->user_data);
-
+    if ((entry->user_data != stderr) && (entry->user_data != stdout)) {
+      fclose (entry->user_data);
+    }
     g_slice_free (LogFuncEntry, entry);
     cleanup = g_slist_delete_link (cleanup, cleanup);
   }
